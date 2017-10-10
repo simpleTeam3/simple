2ffdf657fb74b5c55f32dfaf28f1888a3bbf4f3f
webproblem <yqx_emails@163.com>
2017-10-10 23:39:54 +0800

    更新示例

--- examples/app.vue
+++ examples/app.vue
@@ -1,11 +1,34 @@
 <style lang="sass">
     @import "../src/style/index.scss";
+	.content{
+		width: 85%;
+		margin: 30px auto;
+	}
+	.demo-list{
+		margin: 30px 0px;
+	}
+	.title{
+		margin-bottom: 18px;
+		height: 50px;
+		line-height: 20px;
+		padding: 15px;
+		border-color: #e2e2e2;
+		border-style: solid;
+    	border-width: 1px 1px 1px 5px;
+	}
 </style>
 <template>
 	<div class="content">
-	    <span>你怎么说嘛</span>
-        <vut-input type="number"></vut-input> 
-        <vutButton></vutButton>
+        <div class="demo-list">
+		    <div class="title">input组件实例</div>
+			<vut-input></vut-input>
+			<vut-input size="large"></vut-input>
+			<vut-input size="small"></vut-input>
+		</div> 
+        <div class="demo-list">
+			 <div class="title">button组件实例</div>
+			<vutButton></vutButton>
+		</div>
 	</div>
 </template>
 <script>
