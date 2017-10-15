0640695bd64db4d3d5285185775421cb5e57fe47
webproblem <yqx_emails@163.com>
2017-10-15 20:24:25 +0800

    update

--- examples/routers/input.vue
+++ examples/routers/input.vue
@@ -15,22 +15,47 @@
 		border-style: solid;
     	border-width: 1px 1px 1px 5px;
 	}
+	.demo-input{width: 200px;}
 </style>
 <template>
-	<div class="content">
-        <div class="demo-list">
-		    <div class="title">input组件实例</div>
-			<vut-input placeholder=""></vut-input>
+	<div>
+		<div class="demo-list">
+			<div class="title">input组件实例</div>
+			<vut-input readonly></vut-input>
 			<vut-input size="large"></vut-input>
 			<vut-input size="small" disabled></vut-input>
 			<vut-input placeholder="测试组件" type="textarea"></vut-input>
-		</div> 
+			<p>
+				<vut-input 
+				placeholder="按回车键试试" 
+				hideborder 
+				@on-enter="enterFn" 
+				@on-focus="focusFn"
+				@on-blur="blurFn"
+				class="demo-input"
+				></vut-input>
+			</p>
+		</div>
+		<div class="demo-list">
+			<div class="title">API</div>
+		</div>
 	</div>
 </template>
 <script>
 	export default {
 		data(){
 			return {
+			}
+		},
+		methods: {
+			enterFn() {
+				alert("组件测试");
+			},
+			focusFn() {
+				console.log("获取了焦点");
+			},
+			blurFn() {
+				console.log("失去了焦点");
 			}
 		}
 	}
