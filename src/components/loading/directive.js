fdc2544fe60a70a81c8f69a511e3cd1684049bf5
liye <550275752@qq.com>
2018-07-27 22:44:59 +0800

    loading相关

--- src/components/loading/directive.js
+++ src/components/loading/directive.js
@@ -7,12 +7,34 @@ export default {
         if(Vue.prototype.$isServer) return;
 
         let insertDom = (parent, el, binding) =>{
+            if(!el.domVisible && getOneStyle(el, 'display') !== 'none' && getOneStyle(el, 'visibility') !== 'hidden'){
+                parent.appendChild(el);
+			
+				if(el.originalPosition !== 'absolute' && el.originalPosition !== 'fixed'){
+					parent.style.position = 'relative';
+				}
 
+                el.instance.visible = true;
+                Vue.nextTick(() => {
+                    el.instance.visible = true;
+				})
+				el.domInsert = true;
+            }
         }
         
         Vue.directive('loading', {
             bind : function(el, binding) {
+                let load = new Load({
+                    el : document.createElement('div'),
+                    data : {
+                        text : el.getAttribute('loading-text'),
+                        fullscreen : !!binding.modifires.fullscreen
+                    }
+                })
 
+                el.instance = load;
+                el.mask = load.$el;
+                
             } 
         })
     }
