d2972d2249056150a6366513835ec096234ae952
Sauce <550275752@qq.com>
2018-07-31 22:42:15 +0800

    Button多样式

--- examples/routers/button.vue
+++ examples/routers/button.vue
@@ -2,6 +2,10 @@
 	<div class="demo-list">
             <div class="title">button组件实例</div>
         <vut-button>普通按钮</vut-button>
+		<vut-button type="primary">主要按钮</vut-button>
+		<vut-button type="success">成功按钮</vut-button>
+		<vut-button type="warning">警告按钮</vut-button>
+		<vut-button type="error">错误按钮</vut-button>
     </div>
 </template>
 <script>
