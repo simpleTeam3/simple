64bc968d058f15952af5add4232c153f93edec0a
liye <550275752@qq.com>
2018-09-19 21:02:10 +0800

    tab提交

---
+++ src/components/tabs/index.js
@@ -0,0 +1,4 @@
+import Tabs from './tabs';
+import TabPanel from './panel';
+Tabs.Panel = TabPanel;
+export default Tabs;
---
+++ src/components/tabs/tabs.vue
@@ -0,0 +1,13 @@
+<template>
+    <div :class="prefix">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'vutTabs',
+    data(){
+        prefix: this.prefix + 'tabs'
+    }
+}
+</script>
