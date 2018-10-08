97815f432a669a729564e100b12a566b08951334
Sauce <550275752@qq.com>
2018-10-08 18:00:50 +0800

    更新notification样式

--- src/components/notification/notification.vue
+++ src/components/notification/notification.vue
@@ -1,15 +1,42 @@
 <template>
-    <div :class="prefix">
-
+    <div :class="[prefix, this.position]">
+        <i></i>
+        <div :class="prefix + '-wrap'">
+            <h3 :class="prefix + '-header'">{{title}}</h3>
+            <div :class="prefix + '-content'">
+                <slot>
+                    <p>{{content}}</p>
+                </slot>
+            </div>
+        </div>
+        <i class="vut-icon-close"></i>
     </div>
 </template>
 
 <script>
 export default {
     name: 'vutNotification',
+    props: {
+        title: {
+            type: String,
+            default: "这是一个title"
+        },
+        content: {
+            type: String,
+            default: "这是一个content有很多的内容，而且可以关闭"
+        },
+        position: {
+            type: String,
+            default: 'right'
+        }
+    },
     data(){
         return {
-            prefix: this.prefix + 'notification'
+            prefix: this.global.prefix + 'notification'
+        }
+    },
+    computed: {
+        notificationClass(){
         }
     }
 }
