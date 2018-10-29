5f0700aa960d170fbf59bade3931dbcff596adb6
Sauce <550275752@qq.com>
2018-10-29 18:47:05 +0800

    modify carousel left

--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -7,11 +7,21 @@
 </template>
 
 <script>
+import { getOneStyle } from '../../utils/tools.js'
+
 export default {
     name: 'vutCarousel',
     data(){
         return {
             prefix: this.global.prefix + 'carousel'
+        }
+    },
+    computed: {
+        itemLength(){
+            return this.$children.length;
+        },
+        itemWidth(){
+            return parseInt(getOneStyle(this.$children[0], 'width'))
         }
     }
 }
