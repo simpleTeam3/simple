84468cf9878ac653ce7c70a44ed72e011aabdf94
Sauce <550275752@qq.com>
2018-12-24 21:03:16 +0800

    [Carousel] item样式更改

--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -13,7 +13,9 @@ export default {
     name: 'vutCarousel',
     data(){
         return {
-            prefix: this.global.prefix + 'carousel'
+            prefix: this.global.prefix + 'carousel',
+            items: [],
+            activeIndex: -1,
         }
     },
     computed: {
@@ -23,6 +25,24 @@ export default {
         itemWidth(){
             return parseInt(getOneStyle(this.$children[0], 'width'))
         }
+    },
+    mounted(){
+        this.updateItems();
+    },
+    watch: {
+        activeIndex(newVal, oldVal){
+            this.resetItemPosition(oldVal);
+        }
+    },
+    methods: {
+        //获取子类目标
+        updateItems(){
+            this.items = this.$children.filter(item => item.$options.name == "vutCarouselItem");
+        },
+        //更新item位置
+        resetItemPosition(oldIndex){
+
+        },
     }
 }
 </script>
