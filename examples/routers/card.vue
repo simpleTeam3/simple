576a0ac56af1a17e9510a838cfd87263ce23495b
Sauce <550275752@qq.com>
2019-01-08 20:27:56 +0800

    [card]增加阴影显示

--- examples/routers/card.vue
+++ examples/routers/card.vue
@@ -1,13 +1,61 @@
 <template>
-    <vut-card></vut-card>
+    <div>
+        <div>
+            <vut-card :cardStyle="style">
+                <p slot="header">我是标题</p>
+                <div>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                </div>
+            </vut-card>
+            <vut-card :cardStyle="style" hoverabled>
+                <p slot="header">我是标题</p>
+                <div>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                </div>
+            </vut-card>
+        </div>
+        <div>
+            <vut-card :cardStyle="style">
+                <div>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                </div>
+            </vut-card>
+            <vut-card :cardStyle="style" hoverabled>
+                <p slot="header">我是标题</p>
+                <div>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                    <p>我是内容</p>
+                </div>
+            </vut-card>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            style: {
+                width: "280px"
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+.vut-card{
+    margin-right: 20px;
+}
 </style>
--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,9 +1,36 @@
 <template>
-    <div></div>
+    <div :class="cardClass" :style="cardStyle">
+        <div :class="prefix + '-header'" v-if="$slots.header">
+            <slot name="header"></slot>
+        </div>
+        <div :class="prefix + '-content'">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'vutCard'
+    name: 'vutCard',
+    props: {
+        cardStyle: {},
+        bodyStyle: {},
+        hoverabled: Boolean
+    },
+    data(){
+        return {
+            prefix: this.global.prefix + 'card'
+        }
+    },
+    computed: {
+        cardClass(){
+            return [
+                this.prefix,
+                {
+                    'hover-shadow': this.hoverabled
+                }
+            ]
+        }
+    },
 }
 </script>
