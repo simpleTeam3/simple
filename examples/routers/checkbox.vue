bdc6cef305ba717fc1dfe4aa3c5cc65270a38944
Sauce <550275752@qq.com>
2019-08-06 20:00:07 +0800

    [checkbox]修复使用 Group相关bug

--- examples/routers/checkbox.vue
+++ examples/routers/checkbox.vue
@@ -3,7 +3,7 @@
         <vut-checkbox v-model="checked1">美丽</vut-checkbox>
         <vut-checkbox v-model="checked2">世界</vut-checkbox>
         <vut-checkbox v-model="checked3">孤儿</vut-checkbox>
-        <vut-checkbox-group>
+        <vut-checkbox-group v-model="most">
             <vut-checkbox label="life">生活</vut-checkbox>
             <vut-checkbox label="cause">因你</vut-checkbox>
             <vut-checkbox label="fire">而火热</vut-checkbox>
@@ -17,7 +17,8 @@ export default {
         return {
             checked1: true,
             checked2: false,
-            checked3: false
+            checked3: false,
+            most: []
         }
     }
 }
