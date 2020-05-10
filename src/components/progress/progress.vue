c343a830b9babcfcc7028cbaabd4eec926e11923
Sauce <550275752@qq.com>
2020-05-10 10:57:28 +0800

    feat: progress add circle

--- src/components/progress/progress.vue
+++ src/components/progress/progress.vue
@@ -1,9 +1,19 @@
 <template>
-    <div :class="prefix">
+    <div :class="[prefix, prefix + '-type-' + type]">
         <div :class="prefix + '-bar-wrap'" v-if="type === 'line'">
             <div :class="prefix + '-bar'" :style="barStyle"></div>
         </div>
-        <div :class="prefix + '-circle'" v-else></div>
+        <div :class="prefix + '-circle'" :style="circleStyle" v-else>
+            <svg viewBox="0 0 100 100">
+                <path
+                    class="el-progress-circle__track"
+                    :d="trackPath"
+                    stroke="#e5e9f2"
+                    :stroke-width="relativeStrokeWidth"
+                    fill="none"
+                    :style="trailPathStyle"></path>
+            </svg>
+        </div>
         <div :class="labelClass">
             <span v-if="percent < 100">{{percent}}%</span>
             <i v-else class="vut-icon-roundcheckfill"></i>
@@ -22,6 +32,14 @@ export default {
         type: {
             type: String,
             default: 'line'
+        },
+        width: {
+            type: Number,
+            default: 130
+        },
+        relativeStrokeWidth: {
+            type: Number,
+            default: 5
         }
     },
     data(){
@@ -43,8 +61,34 @@ export default {
             }
             return style;
         },
+        circleStyle() {
+            return {width: this.width + 'px', height: this.width + 'px'}
+        },
         percent(){
             return this.value <= 100 ? Math.floor(this.value) : 100;
+        },
+        radius() {
+            if (this.type === 'circle' || this.type === 'dashboard') {
+                return parseInt(50 - parseFloat(this.relativeStrokeWidth) / 2, 10);
+            } else {
+                return 0;
+            }
+        },
+        trackPath() {
+            const radius = this.radius;
+            const isDashboard = this.type === 'dashboard';
+            return `
+                M 50 50
+                m 0 ${isDashboard ? '' : '-'}${radius}
+                a ${radius} ${radius} 0 1 1 0 ${isDashboard ? '-' : ''}${radius * 2}
+                a ${radius} ${radius} 0 1 1 0 ${isDashboard ? '' : '-'}${radius * 2}
+            `;
+        },
+        trailPathStyle() {
+            return {
+            strokeDasharray: `${(this.perimeter * this.rate)}px, ${this.perimeter}px`,
+            strokeDashoffset: this.strokeDashoffset
+            };
         },
     }
 }
